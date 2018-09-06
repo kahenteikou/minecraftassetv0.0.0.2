8b14d58fd3c26f9acc39ac0ac1bba9ee09a3065b
kokkiemouse <39451248+kokkiemouse@users.noreply.github.com>
2018-09-07 06:15:43 +0900

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCAssetsDOWNA;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Error\nオプションを指定してください。");
+                return;
+
+            }
+            if (args.Length == 1)
+            {
+                Console.WriteLine("Error\nオプションを指定してください。");
+                return;
+
+            }
+            string outpath3 = args[1].TrimEnd('\\','"');
+            Console.Write(outpath3);
+            Console.WriteLine();
+            Core ASCore = new Core();
+            ASCore.Assets(args[0],outpath3);
+
+        }
+    }
+}
---
+++ ConsoleApp1/bin/Release/CA.cmd
@@ -0,0 +1,2 @@
+ConsoleApp1.exe "1.12" "C:\Users\kokki\source\repos\MCAssetsDOWNA\ConsoleApp1\bin\Release\testdir"
+pause
